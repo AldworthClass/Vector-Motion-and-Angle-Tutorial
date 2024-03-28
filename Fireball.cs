c118568ba9e6d115a79d6882076f8fda296f1cdd
Steve Aldworth <s.aldworth@tvdsb.ca>
2024-03-28 15:14:47 -0400

    implemented fireballs, but not done

--- Fireball.cs
+++ Fireball.cs
@@ -10,9 +10,38 @@ namespace Vector_Motion_and_Angle_Tutorial
 {
     public class Fireball
     {
-        Texture2D _texture;
-        Rectangle _bounds;
-        Vector2 _location;
-        Vector2 _speed;
+        private Texture2D _texture;
+        private Rectangle _rect;
+        private Vector2 _location;
+        private Vector2 _direction;
+        private int _speed;
+        private int _size;
+
+        public Fireball(Texture2D texture, Vector2 location, Vector2 target)
+        {
+            _size = 10;
+            _texture = texture;
+            _location = location;
+            _rect = new Rectangle(location.ToPoint(), new Point(_size, _size));
+            _direction = target - location;
+            _direction.Normalize();
+            _speed = 2;
+        }
+
+        public Rectangle Rect
+        {
+            get { return _rect; }
+        }
+
+        public void Update()
+        {
+            _location += _direction * _speed;
+            _rect.Location = _location.ToPoint();
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(_texture, _rect, Color.White);
+        }
     }
 }
--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
 
 namespace Vector_Motion_and_Angle_Tutorial
 {
@@ -11,8 +12,11 @@ namespace Vector_Motion_and_Angle_Tutorial
 
         Texture2D tankTexture;
         Rectangle tankRect;
-        Vector2 tankLocation;
         Vector2 tankSpeed;
+
+        List<Fireball> fireballs;
+
+        Rectangle window;
 
         Texture2D fireballTexture;
         Rectangle fireballRect;
@@ -33,7 +37,9 @@ namespace Vector_Motion_and_Angle_Tutorial
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
+            window = new Rectangle(0, 0, _graphics.PreferredBackBufferWidth, _graphics.PreferredBackBufferHeight);
+            tankRect = new Rectangle(350, 250, 75, 75);
+            fireballs = new List<Fireball>();
             base.Initialize();
         }
 
@@ -42,14 +48,34 @@ namespace Vector_Motion_and_Angle_Tutorial
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
+            fireballTexture = Content.Load<Texture2D>("fireball");
+            tankTexture = Content.Load<Texture2D>("tank");
+            hitBoxTexture = Content.Load <Texture2D>("rectangle");
         }
 
         protected override void Update(GameTime gameTime)
         {
+            Window.Title = fireballs.Count + "";
+            prevMouseState = mouseState;
+            mouseState = Mouse.GetState();
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
             // TODO: Add your update logic here
+            if (mouseState.LeftButton == ButtonState.Pressed)
+            {
+                fireballs.Add(new Fireball(fireballTexture, tankRect.Center.ToVector2(), mouseState.Position.ToVector2()));
+            }
+
+            for (int i = 0; i < fireballs.Count; i++)
+            {
+                fireballs[i].Update();
+                if (!window.Intersects(fireballs[i].Rect)) // removes fireballs after they leave the window
+                {
+                    fireballs.RemoveAt(i);
+                    i--;
+                }
+            }
 
             base.Update(gameTime);
         }
@@ -59,6 +85,15 @@ namespace Vector_Motion_and_Angle_Tutorial
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(hitBoxTexture, tankRect, Color.White);
+            _spriteBatch.Draw(tankTexture, tankRect, Color.White);
+
+            foreach (Fireball fireball in fireballs)
+                fireball.Draw(_spriteBatch);
+
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
