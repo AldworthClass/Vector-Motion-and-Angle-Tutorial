4ce79687c8ca2ef1a7eaaf9a44387e628a7d4bd6
Steve Aldworth <s.aldworth@tvdsb.ca>
2024-03-28 14:17:03 -0400

    Add project files.

---
+++ Fireball.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vector_Motion_and_Angle_Tutorial
+{
+    public class Fireball
+    {
+        Texture2D _texture;
+        Rectangle _bounds;
+        Vector2 _location;
+        Vector2 _speed;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,3 @@
+﻿
+using var game = new Vector_Motion_and_Angle_Tutorial.Game1();
+game.Run();
